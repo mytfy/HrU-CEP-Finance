7210184249ab4ec71e53b28a17936b33689783ec
Abdulfetah Suudi <152389916+mytfy@users.noreply.github.com>
2023-12-23 11:26:58 +0300

    Declare variables of the class

--- Payment.java
+++ Payment.java
@@ -4,4 +4,58 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Payment extends javax.swing.JFrame {
+
+    private JFrame frame; 
+
+    int Xmouse;
+    int Ymouse;
+    // Variables declaration 
+    private javax.swing.JLabel Batch;
+    private javax.swing.JScrollPane Bg_ScrollPane2;
+    private javax.swing.JLabel Department;
+    private javax.swing.JLabel RegNo;
+    private javax.swing.JButton Reset;
+    private javax.swing.JLabel Roll;
+    private javax.swing.JLabel Student_Name;
+    private javax.swing.JTextField AdmissionField;
+    private javax.swing.JTextArea area;
+    private javax.swing.JTextField BatchNoField;
+    private javax.swing.JPanel body;
+    private javax.swing.JTextField DepartmentField;
+    private javax.swing.JTextField ExaminationField;
+    private javax.swing.JTextField FineLateField;
+    private javax.swing.JButton getrecipt;
+    private javax.swing.JTextField ImprovementFeild;
+    private javax.swing.JPanel items;
+    private javax.swing.JCheckBox AdmissionFeeCheck;
+    private javax.swing.JCheckBox ReAdmissionCheck;
+    private javax.swing.JCheckBox SemesterCheck;
+    private javax.swing.JCheckBox LabCheck;
+    private javax.swing.JCheckBox LibraryCheck;
+    private javax.swing.JCheckBox ExaminationCheck;
+    private javax.swing.JCheckBox ImprovementFee;
+    private javax.swing.JCheckBox MArksSheetCheck;
+    private javax.swing.JCheckBox FineLateCheck;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField LabFeeFeild;
+    private javax.swing.JTextField LibraryFeeFeild;
+    private javax.swing.JLabel logout;
+    private javax.swing.JLabel minimize;
+    private javax.swing.JTextField MarksSheetField;
+    private javax.swing.JScrollPane print_area;
+    private javax.swing.JButton printrecipt;
+    private javax.swing.JTextField ReAdmissionField;
+    private javax.swing.JTextField RollNoField;
+    private javax.swing.JTextField RegNoField;
+    private java.awt.ScrollPane scrollPane1;
+    private javax.swing.JTextField SemesterField;
+    private javax.swing.JTextField StudentNaameField;
+    private javax.swing.JPanel taskbar;
+    private javax.swing.JTextField TotalCost;
+    private javax.swing.JLabel text;
+    private javax.swing.JButton total;
+    private javax.swing.JTextField totalcost;
+    private javax.swing.JLabel Exit;
+    // End of variables declaration
 }
