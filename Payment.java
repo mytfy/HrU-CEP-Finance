83367977bec210a39198f9641f91e9108ede3c67
Abdulfetah Suudi <152389916+mytfy@users.noreply.github.com>
2023-12-23 11:29:08 +0300

    Create initComponents file
    Initialize variables in this method

--- Payment.java
+++ Payment.java
@@ -58,4 +58,56 @@ public class Payment extends javax.swing.JFrame {
     private javax.swing.JTextField totalcost;
     private javax.swing.JLabel Exit;
     // End of variables declaration
+
+        private void initComponents() {
+
+        // initialize the variables
+        jPanel1 = new javax.swing.JPanel();
+        taskbar = new javax.swing.JPanel();
+        minimize = new javax.swing.JLabel();
+        Exit = new javax.swing.JLabel();
+        text = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        scrollPane1 = new java.awt.ScrollPane();
+        body = new javax.swing.JPanel();
+        Bg_ScrollPane2 = new javax.swing.JScrollPane();
+        items = new javax.swing.JPanel();
+        AdmissionFeeCheck = new javax.swing.JCheckBox();
+        ReAdmissionCheck = new javax.swing.JCheckBox();
+        SemesterCheck = new javax.swing.JCheckBox();
+        LabCheck = new javax.swing.JCheckBox();
+        LibraryCheck = new javax.swing.JCheckBox();
+        ExaminationCheck = new javax.swing.JCheckBox();
+        ImprovementFee = new javax.swing.JCheckBox();
+        MArksSheetCheck = new javax.swing.JCheckBox();
+        FineLateCheck = new javax.swing.JCheckBox();
+        ReAdmissionField = new javax.swing.JTextField();
+        SemesterField = new javax.swing.JTextField();
+        LabFeeFeild = new javax.swing.JTextField();
+        LibraryFeeFeild = new javax.swing.JTextField();
+        ExaminationField = new javax.swing.JTextField();
+        ImprovementFeild = new javax.swing.JTextField();
+        MarksSheetField = new javax.swing.JTextField();
+        FineLateField = new javax.swing.JTextField();
+        AdmissionField = new javax.swing.JTextField();
+        logout = new javax.swing.JLabel();
+        Student_Name = new javax.swing.JLabel();
+        Batch = new javax.swing.JLabel();
+        RegNo = new javax.swing.JLabel();
+        Roll = new javax.swing.JLabel();
+        Department = new javax.swing.JLabel();
+        StudentNaameField = new javax.swing.JTextField();
+        RegNoField = new javax.swing.JTextField();
+        BatchNoField = new javax.swing.JTextField();
+        DepartmentField = new javax.swing.JTextField();
+        RollNoField = new javax.swing.JTextField();
+        totalcost = new javax.swing.JTextField();
+        TotalCost = new javax.swing.JTextField();
+        Reset = new javax.swing.JButton();
+        total = new javax.swing.JButton();
+        getrecipt = new javax.swing.JButton();
+        printrecipt = new javax.swing.JButton();
+        print_area = new javax.swing.JScrollPane();
+        area = new javax.swing.JTextArea();
+    }
 }
