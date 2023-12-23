bc3dae87c52d035738a3de2251abf2961348678e
Adn <152364726+Adnantii@users.noreply.github.com>
2023-12-23 22:25:02 +0300

    Update new form dashboard

--- DashBoard.java
+++ DashBoard.java
@@ -23,6 +23,87 @@ public class DashBoard extends javax.swing.JFrame {
         this.setLocationRelativeTo(null);
     }
 
+          private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        exit = new javax.swing.JLabel();
+        logout = new javax.swing.JLabel();
+        payment = new javax.swing.JLabel();
+        Data = new javax.swing.JLabel();
+        register = new javax.swing.JLabel();
+        options = new javax.swing.JLabel();
+        dashboard = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        jPanel1.setLayout(null);
+
+        exit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        exit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                exitMouseClicked(evt);
+            }
+        });
+        jPanel1.add(exit);
+        exit.setBounds(770, 460, 30, 30);
+
+        logout.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        logout.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                logoutMouseClicked(evt);
+            }
+        });
+        jPanel1.add(logout);
+        logout.setBounds(10, 460, 110, 30);
+
+        payment.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        payment.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                paymentMouseClicked(evt);
+            }
+        });
+        jPanel1.add(payment);
+        payment.setBounds(390, 350, 180, 30);
+
+        Data.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        Data.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DataMouseClicked(evt);
+            }
+        });
+        jPanel1.add(Data);
+        Data.setBounds(440, 230, 180, 30);
+
+        register.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        register.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                registerMouseClicked(evt);
+            }
+        });
+        jPanel1.add(register);
+        register.setBounds(390, 110, 180, 30);
+
+        jPanel1.add(options);
+        options.setBounds(90, 70, 600, 380);
+
+        dashboard.setIcon(new javax.swing.ImageIcon(getClass().getResource("images/Dashboard.jpg")));
+        jPanel1.add(dashboard);
+        dashboard.setBounds(0, 0, 811, 499);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 808,
+                                javax.swing.GroupLayout.PREFERRED_SIZE));
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 496, Short.MAX_VALUE));
+
+        pack();
+    }
+
 
 
 
