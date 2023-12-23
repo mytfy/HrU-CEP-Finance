4f1f3b4ca71164db7bceeadd236523abdb2e0f16
Adn <152364726+Adnantii@users.noreply.github.com>
2023-12-23 22:06:13 +0300

    Add variables declaration

--- DashBoard.java
+++ DashBoard.java
@@ -4,4 +4,18 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class DashBoard extends javax.swing.JFrame {
+      // Variables declaration 
+    private javax.swing.JLabel Data;
+    private javax.swing.JLabel dashboard;
+    private javax.swing.JLabel exit;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel logout;
+    private javax.swing.JLabel options;
+    private javax.swing.JLabel payment;
+    private javax.swing.JLabel register;
+    // End of variables declaration
+
+    /**
+     * Creates new form DashBoard
+     */
 }
