f77d4c993a65f90e36a39c7e4022b49ab21ece35
Adn <152364726+Adnantii@users.noreply.github.com>
2023-12-23 22:29:18 +0300

    Set the nimbus look feel

--- DashBoard.java
+++ DashBoard.java
@@ -129,6 +129,28 @@ public class DashBoard extends javax.swing.JFrame {
         JOptionPane.showMessageDialog(null, "This option is underdevelopment");
     }
 
+      public static void main(String args[]) {
+        /*
+         * Set the Nimbus look and feel
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+            
+
 
 
 
