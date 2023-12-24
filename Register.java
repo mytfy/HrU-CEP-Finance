56ad58cd7258104e0acf70db6e8bc687d5530638
ademkedirGaliyo <152389410+ademkedirGaliyo@users.noreply.github.com>
2023-12-24 08:46:51 +0300

    Creates new Register form

--- Register.java
+++ Register.java
@@ -25,7 +25,23 @@ public class Register extends javax.swing.JFrame {
     private javax.swing.JLabel resgister;
     // End of variables declaration
 
-
+    /**
+     * Creates new form Register
+     */
+    public Register() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        jPanel1.setFocusable(true);
+        jPanel1.setBackground(new Color(0, 0, 0, 0));
+        IDNumText.setBackground(new Color(0, 0, 0, 0));
+        NameText.setBackground(new Color(0, 0, 0, 0));
+        EmailText.setBackground(new Color(0, 0, 0, 0));
+        LocationText.setBackground(new Color(0, 0, 0, 0));
+        DepartmentText.setBackground(new Color(0, 0, 0, 0));
+        PhoneNumberText.setBackground(new Color(0, 0, 0, 0));
+        BatchNumText.setBackground(new Color(0, 0, 0, 0));
+        RollNumText.setBackground(new Color(0, 0, 0, 0));
+    }
 
   // **********
 }
