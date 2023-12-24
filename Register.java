fb64c3a5e1ac61ac843f7b6abce3b179b3654f33
ademkedirGaliyo <152389410+ademkedirGaliyo@users.noreply.github.com>
2023-12-24 09:44:14 +0300

    Set Nimbus look and feel and initialize main GUI window

--- Register.java
+++ Register.java
@@ -531,5 +531,37 @@ private void DepartmentTextFocusLost(java.awt.event.FocusEvent evt) {
     }
 }
 
+       public static void main(String args[]) {
+        /*
+         * Set the Nimbus look and feel
+         */
+        try {
+              // Set the Nimbus look and feel
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Register.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Register.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Register.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Register.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /*
+         * Create and display the form
+         */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                new Register().setVisible(true);
+            }
+        });
+    }
   // **********
 }
