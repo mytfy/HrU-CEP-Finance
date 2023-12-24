643414362b87ebb9571e66903c5a2aa4c2eedebf
AbukiM0 <133163591+AbukiM0@users.noreply.github.com>
2023-12-24 09:24:21 +0300

    add unit testing / main function

--- Login.java
+++ Login.java
@@ -128,4 +128,37 @@ public class Login extends javax.swing.JFrame {
             password.setText(null);
         }
     }
+     // add unit testing / main function
+    public static void main(String args[]) {
+        /*
+         * Set the Nimbus look and feel
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /*
+         * Create and display the form
+         */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+
 }
