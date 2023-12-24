193559a7af4c7466f23eb89fbedfc90636d42b32
ademkedirGaliyo <152389410+ademkedirGaliyo@users.noreply.github.com>
2023-12-24 08:45:47 +0300

    declare field for register class

--- Register.java
+++ Register.java
@@ -5,4 +5,27 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Register extends javax.swing.JFrame {
+      private Component frame;
+    int Xmouse;
+    int Ymouse;
+
+    // Variables declaration 
+    private javax.swing.JTextField IDNumText;
+    private javax.swing.JTextField NameText;
+    private javax.swing.JTextField EmailText;
+    private javax.swing.JTextField LocationText;
+    private javax.swing.JTextField DepartmentText;
+    private javax.swing.JTextField PhoneNumberText;
+    private javax.swing.JTextField BatchNumText;
+    private javax.swing.JTextField RollNumText;
+    private javax.swing.JLabel exit;
+    private javax.swing.JLabel background;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel minimize;
+    private javax.swing.JLabel resgister;
+    // End of variables declaration
+
+
+
+  // **********
 }
