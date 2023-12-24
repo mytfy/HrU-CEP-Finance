1a6444751fe752179ba4ab5ab4d28c25ea5dd1c2
BosBak <129318651+Bakerboonsa11@users.noreply.github.com>
2023-12-24 12:57:04 +0300

    Disable unit testing

--- DashBoard.java
+++ DashBoard.java
@@ -138,34 +138,35 @@ public class DashBoard extends javax.swing.JFrame {
     private void DataMouseClicked(java.awt.event.MouseEvent evt) {
         JOptionPane.showMessageDialog(null, "This option is underdevelopment");
     }
-      // unit testing
-      public static void main(String args[]) {
-        /*
-         * Set the Nimbus look and feel
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.ororlogging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+      
+      // // unit testing
+      // public static void main(String args[]) {
+      //   /*
+      //    * Set the Nimbus look and feel
+      //    */
+      //   try {
+      //       for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+      //           if ("Nimbus".equals(info.getName())) {
+      //               javax.swing.UIManager.setLookAndFeel(info.getClassName());
+      //               break;
+      //           }
+      //       }
+        // } catch (ClassNotFoundException ex) {
+        //     java.util.ororlogging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        // } catch (InstantiationException ex) {
+        //     java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        // } catch (IllegalAccessException ex) {
+        //     java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        // } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        //     java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        // }
 
             
-        java.awt.EventQueue.invokeLater(new Runnable() {
-
-            public void run() {
-                new DashBoard().setVisible(true);
-            }
-        });
-    }
+        // java.awt.EventQueue.invokeLater(new Runnable() {
+      // 
+    //         public void run() {
+    //             new DashBoard().setVisible(true);
+    //         }
+    //     });
+    // }
 }
