882c8930673eeaff1281891f0860a75f5115ea23
AbukiM0 <133163591+AbukiM0@users.noreply.github.com>
2023-12-24 08:55:38 +0300

    Declare variables

--- Login.java
+++ Login.java
@@ -4,11 +4,21 @@ import javax.swing.JOptionPane;
 import java.awt.*;
 
 public class Login extends javax.swing.JFrame {
-  /**
+    /**
      * Creates new form Login
      */
     public Login() {
         initComponents();
         this.setLocationRelativeTo(null);
     }
+
+    // Variables declaration
+    private javax.swing.JLabel background;
+    private javax.swing.JLabel exit;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel loinbuttton;
+    private javax.swing.JLabel minimize;
+    private javax.swing.JPasswordField password;
+    private javax.swing.JTextField userid;
+    // End of variables declaration
 }
